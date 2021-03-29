63f3613486be0632be987c626a9a1d3f104a5f8f
Eric Lopez <hello@ericlopez.me>
2021-03-30 00:02:28 +0200

    load generic tasks from the ~/.yat directory

--- cmd/yat/main.go
+++ cmd/yat/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"log"
 	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/thisiserico/yat"
 )
 
 func main() {
-	store := yat.NewStore()
+	store := yat.NewFileStore(expandPath("~/.yat"))
 	model := yat.NewUI(store.LoadTasks())
 
 	p := tea.NewProgram(model)
@@ -17,4 +20,15 @@ func main() {
 		log.Println(err)
 		os.Exit(1)
 	}
+}
+
+func expandPath(path string) string {
+	usr, _ := user.Current()
+	dir := usr.HomeDir
+
+	if strings.HasPrefix(path, "~/") {
+		path = filepath.Join(dir, path[2:])
+	}
+
+	return path
 }
--- store.go
+++ store.go
@@ -1,34 +1,72 @@
 package yat
 
-import "time"
+import (
+	"bufio"
+	"io"
+	"os"
+	"regexp"
+	"time"
+)
 
 type Store interface {
 	LoadTasks() tasks
 	SaveTasks(tasks)
 }
 
-type dummyStore struct{}
-
-func NewStore() Store {
-	return &dummyStore{}
+type fileStore struct {
+	file *os.File
 }
 
-func (d *dummyStore) LoadTasks() tasks {
-	return tasks{
-		{
-			summary: "a brand new trask",
-			addedAt: time.Now(),
-		},
-		{
-			summary: "this one is not so cool tho...",
-			addedAt: time.Now(),
-		},
-		{
-			summary:     "but this one is completed!",
-			isCompleted: true,
-			addedAt:     time.Now(),
-		},
+func NewFileStore(file string) Store {
+	f, err := os.OpenFile(file, os.O_CREATE|os.O_RDWR, 0755)
+	if err != nil {
+		panic(err)
+	}
+
+	return &fileStore{
+		file: f,
 	}
 }
 
-func (d *dummyStore) SaveTasks(_ tasks) {}
+func (f *fileStore) LoadTasks() tasks {
+	offset, err := f.file.Seek(0, io.SeekCurrent)
+	if err != nil {
+		return nil
+	}
+
+	scanner := bufio.NewScanner(f.file)
+	scanner.Split(func(data []byte, atEof bool) (advance int, token []byte, err error) {
+		advance, token, err = bufio.ScanLines(data, atEof)
+		if err == nil && token != nil {
+			offset += int64(advance)
+		}
+
+		return advance, token, err
+	})
+
+	var tasks tasks
+	regex, _ := regexp.Compile("^\\[([x ])\\] (.+) \\| (.+)$")
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		if !regex.Match(line) {
+			continue
+		}
+
+		submatches := regex.FindSubmatch(line)
+
+		addedAt := time.Now().UTC()
+		if existingAddedAt, err := time.Parse(time.RFC3339, string(submatches[3])); err == nil {
+			addedAt = existingAddedAt
+		}
+		tasks = append(tasks, &task{
+			summary:     string(submatches[2]),
+			isCompleted: string(submatches[1]) == "x",
+			addedAt:     addedAt,
+		})
+	}
+
+	return tasks
+}
+
+func (f *fileStore) SaveTasks(_ tasks) {
+}
